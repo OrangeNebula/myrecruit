677a175a57c9dc301cf91aab098f1159acaf9323
강지현 <rednebula@mfort.co.kr>
2021-11-07 23:31:11 +0900

    First commit

---
+++ src/test/kotlin/com/my/recruit/myrecruit/MyrecruitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.my.recruit.myrecruit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyrecruitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
